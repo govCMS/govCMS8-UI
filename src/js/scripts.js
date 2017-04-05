ac20fd9785f50b8c38f64b0a3060828d931e13e8
paul.killer <paul.killer@gmail.com>
2017-04-06 09:04:24 +1000

    add file size to file link fields

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,10 @@
 
             // Let's use bootstraps title display as it's much nicer!
             $('main.container [title]').each(function() {
+                if ($(this).parents('.sf-dump, .kint').length) {
+                    // Need this to stop it running on our dump/kint commands which breaks expansions
+                    return;
+                }
                 $(this).attr('data-toggle', 'tooltip');
                 $(this).attr('data-placement', 'top');
             });
