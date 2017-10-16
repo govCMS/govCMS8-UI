4668b19611d17fa534dc4d1c2ffb7193b2c36e93
Jesse Boyd <jesse.boyd@digital.gov.au>
2017-10-16 16:56:52 +1100

    Fixed scripts.js to remove automatic inclusion of Bootstrap tooltip as described in https://github.com/govCMS/govCMS8-core/issues/6#issuecomment-336785525.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,23 @@
 (function ($, Drupal, drupalSettings) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.mybehavior = {
-        attach: function (context, settings) {
-
-            // Let's use bootstraps title display as it's much nicer!
-            $('main.container [title]').each(function() {
-                if ($(this).parents('.sf-dump, .kint').length) {
-                    // Need this to stop it running on our dump/kint commands which breaks expansions
-                    return;
-                }
-                $(this).attr('data-toggle', 'tooltip');
-                $(this).attr('data-placement', 'top');
-            });
-            $('[data-toggle="tooltip"]').tooltip();
-
-        }
-    };
+  Drupal.behaviors.mybehavior = {
+    attach: function (context, settings) {
+      // Test whether the tooltip function exists. It will not if Bootstrap is disabled.
+      if(typeof tooltip === 'function') {
+        // Let's use bootstraps title display as it's much nicer!
+        $('main.container [title]').each(function() {
+          if ($(this).parents('.sf-dump, .kint').length) {
+            // Need this to stop it running on our dump/kint commands which breaks expansions
+            return;
+          }
+          $(this).attr('data-toggle', 'tooltip');
+          $(this).attr('data-placement', 'top');
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+      }
+    }
+  };
 
 })(jQuery, Drupal, drupalSettings);
