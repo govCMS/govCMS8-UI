3c95cf0e244e9235d7a7deca28983ad8b1c7c895
Jesse Boyd <jesse.boyd@digital.gov.au>
2017-10-17 13:14:42 +1100

    Updated gulp files and readme. Added function to catch Twig changes. Added BrowserSync. Removed installation instructions from the gulpfile and added to readme.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,3 @@
-// NOTES:
-// https://markgoodyear.com/2014/01/getting-started-with-gulp/
-// http://www.sitepoint.com/introduction-gulp-js/
-// http://ilikekillnerds.com/2014/11/10-highly-useful-gulp-js-plugins-for-a-super-ninja-front-end-workflow/
-
-// *************************
-// SETUP STEPS:
-//
-// # brew|apt install node
-// # [sudo] npm install -g gulp gulp-jshint gulp-imagemin gulp-notify gulp-autoprefixer gulp-minify-css gulp-compass gulp-uncss gulp-concat gulp-uglify casperjs phantomjs-prebuilt jshint
-// # cd [path]/govcms-theme/govcms-theme/
-// # [sudo] npm link gulp gulp-jshint gulp-imagemin gulp-notify gulp-autoprefixer gulp-minify-css gulp-compass gulp-uncss gulp-concat gulp-uglify jshint
-//
-// Then just run 'gulp' to watch directory for changes :)
-//
-// May encounter this on old Ubuntu: http://stackoverflow.com/questions/32490328/gulp-autoprefixer-throwing-referenceerror-promise-is-not-defined
-// *************************
-
-
 // include gulp
 var gulp = require('gulp');
 // include plug-ins
@@ -30,14 +11,12 @@ var uncss = require('gulp-uncss');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
+var browser = require('browser-sync');
 
 // TODO add https://github.com/johanbrook/gulp-fontcustom
 //          https://docs.npmjs.com/files/package.json
 // Using OS X Iconical for now though,
 // check the DESIGN folder
-// fix up instructions above to take into account npm install.
-
-
 
 // JS minify
 gulp.task('scripts', function() {
@@ -46,8 +25,6 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./js/'));
 });
 
-
-
 // minify new images
 gulp.task('images', function() {
     return gulp.src('./src/img/**/*')
@@ -55,18 +32,15 @@ gulp.task('images', function() {
         .pipe(gulp.dest('./img'))
 });
 
-
-
 // Compile the Sass
 gulp.task('sass', function() {
     gulp.src('./src/sass/colors.scss')
         .pipe(sass({
             noCache: true
         }))
-        .pipe(gulp.dest('./src/styles'));
+        .pipe(gulp.dest('./src/styles'))
+        .pipe(browser.reload({ stream: true }));
 });
-
-
 
 // CSS concat, auto-prefix, optimise and minify
 gulp.task('styles', function() {
@@ -74,14 +48,27 @@ gulp.task('styles', function() {
         .pipe(concat('./colors.css'))
         .pipe(autoprefix('last 2 versions'))
         .pipe(minifyCSS())
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./css/'))
+        .pipe(browser.reload({ stream: true }));
+
 });
 
+// Reload the browser after a change to Twig files. Note this implementation does
+// not do anything yet, need to rebuild cache after a change.
+gulp.task('twig', function() {
+  browser.reload();
+});
 
+// Set up a BrowserSync server to view changes on.
 
+gulp.task('server', function() {
+  browser.init({
+    proxy: 'http://au.gov.dta.dev:8888/'
+  });
+});
 
 // default gulp task
-gulp.task('default', ['images', 'scripts', 'sass', 'styles'], function() {
+gulp.task('default', ['images', 'scripts', 'sass', 'styles', 'server'], function() {
 
     // watch for img optim changes
     gulp.watch('./src/img/*', function() {
@@ -104,6 +91,11 @@ gulp.task('default', ['images', 'scripts', 'sass', 'styles'], function() {
     // watch for CSS changes
     gulp.watch('./src/styles/*.css', function() {
         gulp.start('styles');
+    });
+
+    // watch for Twig changes
+    gulp.watch('./templates/**/*.html.twig', function() {
+        gulp.start('twig');
     });
 
 });
