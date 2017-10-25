d85d656635760b2df27e9de2c5366443d1e106d1
Joseph Zhao <joseph.zhao@xing.net.au>
2017-10-25 18:50:55 +1100

    govCMS VM configs tweaks

--- box/drupal-vm/scripts/post-provision.sh
+++ box/drupal-vm/scripts/post-provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 HOME_PATH="/home/vagrant"
-GOVCMS_UI_PATH="/var/www/govcms-ui"
+GOVCMS_UI_PATH="/var/www/govcms8-ui"
 
 sudo rm -rf /var/www/govcms8/docroot/themes/contrib/govcms-ui
+sudo mkdir -p /var/www/govcms8/docroot/themes/custom
 sudo ln -s $GOVCMS_UI_PATH /var/www/govcms8/docroot/themes/custom/govcms-ui
