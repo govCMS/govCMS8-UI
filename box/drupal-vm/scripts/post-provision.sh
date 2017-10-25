7bcfcfa0bbfce853afc4334112e7ff40bbf24f76
Joseph Zhao <joseph.zhao@xing.net.au>
2017-10-25 21:40:08 +1100

    Update govCMS VM post provision script

--- box/drupal-vm/scripts/post-provision.sh
+++ box/drupal-vm/scripts/post-provision.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-HOME_PATH="/home/vagrant"
 GOVCMS_UI_PATH="/var/www/govcms8-ui"
 
 sudo rm -rf /var/www/govcms8/docroot/themes/contrib/govcms-ui
-sudo mkdir -p /var/www/govcms8/docroot/themes/custom
-sudo ln -s $GOVCMS_UI_PATH /var/www/govcms8/docroot/themes/custom/govcms-ui
+ln -s $GOVCMS_UI_PATH /var/www/govcms8/docroot/themes/contrib/govcms-ui
+sudo chown -R www-data:vagrant /var/www/govcms8/docroot/sites/default/files
